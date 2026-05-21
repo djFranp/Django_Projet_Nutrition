96d7cc8987fca019ddde42b75759580a0ca7c14e
djFranp <prisofrancis@gmail.com>
2026-05-21 21:35:33 +0200

    nouveaux fichiers commités pour prise en compte de git ignore car le précédent n'avait pas marché

--- src/core/utils/validators.py
+++
@@ -1,5 +0,0 @@
-from django.core.validators import RegexValidator
-
-
-LETTER_SPACE_DASH_VALIDATOR = RegexValidator(r'^[a-zA-ZA-y\s-]*$', 'Seules les lettres, espaces '
-                                                                   'et tirets sont autrorisés.')
